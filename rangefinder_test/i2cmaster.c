a77b22946d05640ae55ebe82fdc5d1e0b6fd4b27
andrewmcnair52 <andrew-52@hotmail.com>
2021-03-28 19:25:54 -0400

    asd

--- rangefinder_test/i2cmaster.c
+++ rangefinder_test/i2cmaster.c
@@ -14,9 +14,9 @@
 #define true 1
 #define false 0
 
-#define DEBUG_START false
+#define DEBUG_START true
 #define DEBUG_STOP false
-#define DEBUG_WRITE false
+#define DEBUG_WRITE true
 #define DEBUG_READ false
 
 #define F_CPU 8000000UL
@@ -82,7 +82,7 @@ unsigned char i2c_start(unsigned char address){
         }
     }
     if( timeout == 0 ){
-        debug_str("s: response timed out");
+        debug_str("s: response timed out\n");
         return 1;
     }
 	// check value of TWI Status Register. Mask prescaler bits.
