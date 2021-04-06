1cb362a11c23f2595131de1ae80fca978128e3a3
andrewmcnair52 <andrew-52@hotmail.com>
2021-04-06 10:09:16 -0400

    asd

--- rangefinder_test/main.c
+++ rangefinder_test/main.c
@@ -47,18 +47,53 @@ int main() {
 
 	// Main loop
 	while(1){
-
-
-		debug_str("\n\nenter a character to select an option:\n");
-		debug_str("r: rangefinder test\n");
-		debug_str("t: temperature test\n");
-		debug_str("s: print all addresses on I2C bus\n");
-		debug_str("m: start moving\n");
-		debug_str("c: controller mode\n");
-		debug_str("\n> ");
-
+		
+		/* bluetooth commands
+		s: start move forward
+		l: stop and start left turn
+		r: stop and start right turn
+		b: start move backward
+		e: stop whatever it was doing
+		p: start obstacle detection
+		
+		tests:
+		tr: rangefinder test, output distance values
+		tt: temperature test, read device id
+		ts: TWI bus scan, print all addresses on bus
+		ta: adc test, output converted readings from IR sensors
+		*/
+		
 		//temporary test switch case, controls robot one iteration at a time
 		switch(debug_in()) {
+			
+			case 's':	//start move forward
+			forward();
+			OCR0A = 0x66;
+			OCR2B = 0x66;
+			break;
+			
+			case 'l': //start moving left
+			left();
+			OCR0A = 0xA0;
+			OCR2B = 0xA0;
+			break;
+			
+			case 'r': //start moving right
+			right();
+			OCR0A = 0xA0;
+			OCR2B = 0xA0;
+			break;
+			
+			case 'b': //start moving backwards
+			reverse();
+			OCR0A = 0x66;
+			OCR2B = 0x66;
+			break;
+			
+			case 'e':
+			OCR0A = 0x0;
+			OCR2B = 0x0;
+			break;
 			
 			case 'p':	//test loop
 			if(!rangefinder_initialized) {		//initialize rangefinder if not already done
@@ -68,136 +103,95 @@ int main() {
 			}
 			forward();
 			counter = 0;
-			OCR0A = 0x80;
-			OCR2B = 0x80;
+			OCR0A = 0x60;
+			OCR2B = 0x60;
 			distance = raw_to_cm( read_rangefinder() );
 			while( distance > 30 ) {
 				distance = raw_to_cm( read_rangefinder() );
+				/* debug output
 				debug_dec(counter);
 				debug_str(": distance = ");
 				debug_dec(distance);
 				debug_str("\n");
 				counter++;
+				*/
 			}
 			OCR0A = 0x00;
 			OCR2B = 0x00;
+			break;
+
+			case 't':		//tests
 			
-			break;
-
-			case 'r':							//range finder test
-			if(!rangefinder_initialized) {		//initialize rangefinder if not already done
-				initVL53L0X(1);
-				setMeasurementTimingBudget( 500 * 1000UL );	//500 ms per measurement
-				rangefinder_initialized = true;
-			}
-			debug_str("range data:");
-			debug_dec( read_rangefinder() );
-			debug_str("\n");
-			break;
-
-			case 't':		//temperature test
-			debug_str("\nstarting temperature test\n");
-			i2c_start( ADT7420_ADDRESS | I2C_WRITE );
-			i2c_write( ADT7420_REG_ID );
-			i2c_rep_start( ADT7420_ADDRESS | I2C_READ );
-			uint8_t test = i2c_readNak();
-			i2c_stop();
-
-			if(test != ADT7420_DEFAULT_ID)
-			debug_str("incorrect ID received\n");
-			else debug_str("correct ID received");
-			break;
-
-			case 's':
-			searchI2C();
-			break;
-
-			case 'm':
-			start_move();	//starts adc reading on loop
-			while(1) {
-				if(nav_data_ready) {
-					debug_str("\ndata:: ");
-					debug_hex(nav_data[0],4);
-					debug_str(" ");
-					debug_hex(nav_data[1],4);
-					debug_str(" ");
-					debug_hex(nav_data[2],4);
-					debug_str("\n");
-					debug_str("OCR0A: ");
-					debug_hex(OCR0A,2);
-					debug_str("\n");
-					//nav_rules();				//apply rules
-					nav_data_ready = false;		//reset flag
-					debug_str("\nenter c to read again, i to increase pwm,  or e to end: ");
-					char input = debug_in();
-					if(input=='c') {
-						ADCSRA |= (1 << ADSC);		//start ADC
-					} else if(input=='i') {
-						OCR0A = OCR0A + 50;
-						ADCSRA |= (1 << ADSC);		//start ADC
-					} else {
-						break;
-					}
-				}
-			}
-			stop_move();
-			break;
-
-			case 'c':				//controller mode
-			debug_str("\n\nController mode\n");
-			debug_str("s: start\n");
-			debug_str("l/r: turn left/right\n");
-			debug_str("t: stop\n");
-			debug_str("d: slower 20\n");
-			debug_str("e: end\n");
-
-			//assuming right side is OCR0A and left is OCR2B, swap these if this ends up being wrong
-			bool quit = false;
-			while(!quit) {
-				debug_str("\n> ");
 				switch(debug_in()) {
-					case 's':			//increment right by 20, which turns left
-					forward();
-					OCR0A = 0xA0;
-					OCR2B = 0xA0;
+					
+					case 'r':	//rangefinder test
+					if(!rangefinder_initialized) {		//initialize rangefinder if not already done
+						initVL53L0X(1);
+						setMeasurementTimingBudget( 500 * 1000UL );	//500 ms per measurement
+						rangefinder_initialized = true;
+					}
+					debug_str("range data:");
+					debug_dec( read_rangefinder() );
+					debug_str("\n");
 					break;
-					case 't':
-					OCR0A = 0x00;
-					OCR2B = 0x00;
-					break;
-					case 'l':			//increment left by 20, which turns right
-					left();
-					OCR0A = 0xA0;
-					OCR2B = 0xA0;
-					break;
-					case 'r':			//increment left by 20, which turns right
-					right();
-					OCR0A = 0xA0;
-					OCR2B = 0xA0;
+				
+					case 't':	//temperature test
+					debug_str("\nstarting temperature test\n");
+					i2c_start( ADT7420_ADDRESS | I2C_WRITE );
+					i2c_write( ADT7420_REG_ID );
+					i2c_rep_start( ADT7420_ADDRESS | I2C_READ );
+					uint8_t test = i2c_readNak();
+					i2c_stop();
+					if(test != ADT7420_DEFAULT_ID)
+					debug_str("incorrect ID received\n");
+					else debug_str("correct ID received");
 					break;
 					
-					
-					case 'e':
-					quit = true;
-					OCR0A = 0b00000000;
-					OCR2B = 0b00000000;
+					case 's':	//scan I2C bus
+					searchI2C();
 					break;
-				}
-				debug_str("OCR0A: ");
-				debug_hex(OCR0A, 2);
-				debug_str(" OCR2B:");
-				debug_hex(OCR2B, 2);
-				debug_str("\n");
-			}
-			break;
+					
+					case 'a':
+					start_move();	//starts adc reading on loop
+					while(1) {
+						if(nav_data_ready) {
+							debug_str("\ndata:: ");
+							debug_hex(nav_data[0],4);
+							debug_str(" ");
+							debug_hex(nav_data[1],4);
+							debug_str(" ");
+							debug_hex(nav_data[2],4);
+							debug_str("\n");
+							debug_str("OCR0A: ");
+							debug_hex(OCR0A,2);
+							debug_str("\n");
+							//nav_rules();				//apply rules
+							nav_data_ready = false;		//reset flag
+							debug_str("\nenter c to read again, i to increase pwm,  or e to end: ");
+							char input = debug_in();
+							if(input=='c') {
+								ADCSRA |= (1 << ADSC);		//start ADC
+							} else if(input=='i') {
+								OCR0A = OCR0A + 50;
+								ADCSRA |= (1 << ADSC);		//start ADC
+							} else {
+								break;
+							}
+						}
+					}
+					stop_move();
+					break;
+					
+					default:
+					break;
 
+				}	// end of test case switch
+			break;		//end of test cases
+			
 			default:
 			break;
 
-
-		}
-
-		//_delay_ms(600);		//delay 0.6s
+		}	//end of command handler switch
 
 	}
 	return 0;
