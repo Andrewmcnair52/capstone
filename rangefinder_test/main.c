18219ff8adb2838c05f27d1ab756f1d6d0b6ad5d
andrewmcnair52 <andrew-52@hotmail.com>
2021-04-07 17:43:32 -0400

    asd

--- rangefinder_test/main.c
+++ rangefinder_test/main.c
@@ -110,14 +110,16 @@ int main() {
 				setMeasurementTimingBudget( 500 * 1000UL );	//500 ms per measurement
 				rangefinder_initialized = true;
 			}
-			forward();
 			counter = 0;
+			/*
+			forward();
 			OCR0A = 0x60;
 			OCR2B = 0x60;
+			*/
 			distance = read_rangefinder();
 			while( distance > 200 ) {
 				distance = read_rangefinder();
-				/* debug output
+				// debug output
 				debug_dec(counter);
 				debug_str(": distance = ");
 				debug_dec(distance);
@@ -125,7 +127,6 @@ int main() {
 				debug_hex(distance,4);
 				debug_str("\n");
 				counter++;
-				*/
 			}
 			OCR0A = 0x00;
 			OCR2B = 0x00;
